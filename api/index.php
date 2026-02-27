2c767bfc533540267cc5ffd34c4147c8cc32e610
kevinjoroge <kevoprojects@gmail.com>
2026-02-27 13:16:20 +0300

    vercel

---
+++ api/index.php
@@ -0,0 +1,6 @@
+
+<h2>Select User Role</h2>
+
+<a href="../parent_login.php"><button>Parent</button></a><br><br>
+<a href="../teacher_login.php"><button>Teacher</button></a><br><br>
+<a href="../admin_login.php"><button>Admin</button></a>
